bdaea91572230af338046a5346c05e6f4528d59e
Kevin <kevinjxhn@Kevins-MacBook-Air.local>
2023-12-07 12:13:36 +0530

    Updating Welcome page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -4,28 +4,29 @@ import hero_img from "../assets/home-hero.webp";
 import { Link } from "react-router-dom";
 
 function Welcome() {
-	return (
-		<div className="hero_section">
-			<h1 className="heading_one">
-				Write, plan, share.
-				<br /> With AI at your side.
-			</h1>
-			<p className="hero_para">
-				Notion is the connected workspace where better, faster work happens.
-			</p>
+  return (
+    <div className="hero_section">
+      <h1 className="heading_one">
+        Write, plan, share.
+        <br /> With AI at your side.
+      </h1>
+      <p className="hero_para">
+        Notion is the connected workspace where better, faster work happens.
+      </p>
 
-			<Link to="/login">
-				<button className="get_notion_btn">
-					Get Notion Free <FontAwesomeIcon icon={faArrowRight} />
-				</button>
-			</Link>
-			<img
-				className="login_with_google_img"
-				src={hero_img}
-				alt="Hero_section_img"
-			/>
-		</div>
-	);
+      <Link to="/login">
+        <button className="get_notion_btn">
+          Get Notion Free <FontAwesomeIcon icon={faArrowRight} />
+        </button>
+      </Link>
+      <img
+        className="hero_section_img"
+        src={hero_img}
+        alt="hero image"
+        style={{ marginTop: "50px" }}
+      />
+    </div>
+  );
 }
 
 export default Welcome;
